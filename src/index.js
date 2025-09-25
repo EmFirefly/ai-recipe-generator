63079bb1f4277c062c11edaae397fe989c0910b0
EmFirefly <meganvzyl@gmail.com>
2025-09-25 08:33:34 +0200

    added javascript and minor tweaks to html and css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-let generateButton = document.getElementById("generateButton");
-generateButton.addEventListener("click", function(event) {
+
+function generateRecipe(event) {
     event.preventDefault();
-    let ingredients = document.getElementById("ingredients").value;
-    console.log("Ingredients:", ingredients);
-});
+    alert("Generating recipe...");
+    let recipeCard = document.getElementById('recipe-output');
+    recipeCard.classList.remove('hidden');
+    let dishIngredients = document.getElementById('ingredients').value;
+    let dishType = document.querySelector('.dish-type');
+    let recipeImage = document.querySelector('.recipe-image');
+    let prepTime = document.querySelector('.prep-time');
+    let dishName = document.querySelector('.dish-name');
+
+    let apiUrl = `https://api.apileague.com/search-recipes?api-key=${apiKey}&query=${dishIngredients}`;
+    let apiKey = "c216e869423c458596a2918307079d87";
+}
+    
+let recipeInputForm = document.getElementById('recipeInputForm');
+recipeInputForm.addEventListener('submit', generateRecipe);
