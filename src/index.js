ef46fba2663f373b9a06ae8f9f8d74bbcb3da454
EmFirefly <meganvzyl@gmail.com>
2025-09-25 09:13:07 +0200

    api-problem

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 
 function generateRecipe(event) {
     event.preventDefault();
-    alert("Generating recipe...");
+    let dishIngredients = document.getElementById('ingredients').value;
+    alert(`Generating recipe with ${dishIngredients}...`);
     let recipeCard = document.getElementById('recipe-output');
     recipeCard.classList.remove('hidden');
-    let dishIngredients = document.getElementById('ingredients').value;
+
+    let apiKey = "c343f7ad59a34b869622edabd5cba895";
+    let apiUrl = `https://api.apileague.com/search-recipes?api-key=${apiKey}&query=${dishIngredients}`;
+
+
+    axios.get(apiUrl).then(updateRecipeCard);
+}
+
+function updateRecipeCard(response) {
     let dishType = document.querySelector('.dish-type');
     let recipeImage = document.querySelector('.recipe-image');
     let prepTime = document.querySelector('.prep-time');
     let dishName = document.querySelector('.dish-name');
 
-    let apiUrl = `https://api.apileague.com/search-recipes?api-key=${apiKey}&query=${dishIngredients}`;
-    let apiKey = "c216e869423c458596a2918307079d87";
+    console.log(response.data);
 }
     
 let recipeInputForm = document.getElementById('recipeInputForm');
